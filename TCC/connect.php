708ffcc1ae02202abdde7371584f8fd4bff36f1d
Jefferson <jeffe.scheffer@gmail.com>
2015-11-02 08:20:33 -0200

    insert clientes

---
+++ TCC/connect.php
@@ -0,0 +1,4 @@
+<?php
+$connect 	= mysql_connect('localhost','root','');
+$selectDb 	=mysql_select_db('sistema_tcc', $conect);
+
---
+++ TCC/select_clientes.php
@@ -0,0 +1,3 @@
+<?php
+$connect 	=mysql_connect('localhost','root','');
+$selectDb 	=mysql_select_db('sistema_tcc', $connect) or die (mysql_error());
