6524606b681baaee0c9e44f30bd79d08c08d33bf
Jefferson <jeffe.scheffer@gmail.com>
2015-11-04 07:46:03 -0200

    Erro de comit

---
+++ TCC/connect.php
@@ -0,0 +1,10 @@
+
+
+<?php
+
+$connect 	=mysql_connect('localhost','root','');
+$selectDb 	=mysql_select_db('sistema_tcc', $connect) or die (mysql_error());
+
+return($connect);
+return($selectDb);
+?>
